d898290dfc8587e8da40fb510f7a53ad1f1f07a9
ABINAYA <abinayas017@gmail.com>
2026-09-11 11:27:50 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -2,10 +2,26 @@
 
 # =====================================
 # Swap Space Creation Script
-# Student Name:
-# Roll Number:
+# Student Name:abinaya
+# Roll Number:1U24IT002
 # =====================================
+# Create a 1 GB swap file
+sudo fallocate -l 1G /swapfile
 
+# Set correct permissions
+sudo chmod 600 /swapfile
+
+# Format the file as swap
+sudo mkswap /swapfile
+
+# Enable swap
+sudo swapon /swapfile
+
+# Verify swap space
+sudo swapon --show
+free -h
+
+exit 0
 # Write your commands below
 
 
